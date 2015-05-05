01a4f04e0a738278a12dfe7fae7d116902f30509
Fuad Hasbun <fhasbn3@gatech.edu>
2015-05-04 23:38:58 -0400

    Added parameters and getter methods for an enemy

--- Enemy.java
+++ Enemy.java
@@ -1,3 +1,23 @@
 public class Enemy {
+    private String name;
+    private String damage;
+    private String hp;
 
+    public Enemy(String name, String damage, String hp) {
+        this.name = name;
+        this.damage = damage;
+        this.hp = hp;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDamage() {
+        return damage;
+    }
+
+    public String getHp() {
+        return hp;
+    }
 }
