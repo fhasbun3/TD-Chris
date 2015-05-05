4f8bb96b5e8fd88321158c13aed8b97254e898a2
Fuad Hasbun <fhasbn3@gatech.edu>
2015-05-05 15:53:49 -0400

    Added types of Enemies

---
+++ Cheeseburger.java
@@ -0,0 +1,5 @@
+public class Cheeseburger extends Enemy {
+    public Enemy(String name, String damage, String hp) {
+        super(name, damage, hp);
+    }
+}
---
+++ Drink.java
@@ -0,0 +1,5 @@
+public class Drink extends Enemy {
+    public Drink(String name, String damage, String hp) {
+        super(name, damage, hp);
+    }
+}
