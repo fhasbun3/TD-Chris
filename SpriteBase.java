35b70f70ef8189e33f2a9dab8772fadeabea3d8d
Fuad Hasbun <fhasbn3@gatech.edu>
2015-05-06 10:46:29 -0400

    Added getter methods, a move method and a addToLayer method

--- SpriteBase.java
+++ SpriteBase.java
@@ -19,6 +19,11 @@ public abstract class SpriteBase {
     private double hp;
     private double damage;
 
+    private double width;
+    private double height;
+
+    private boolean canMove;
+
     public SpriteBase(Image spriteImage, Pane layer, double xcomp, double ycomp,
                       double rcomp, double dx, double dy, double dr, double hp,
                       double damage) {
@@ -31,6 +36,69 @@ public abstract class SpriteBase {
         this.dy = dy;
         this.dr = dr;
         this.hp = hp;
-        this.damage = damage;                     
+        this.damage = damage;
+
+        this.spriteImageView = new ImageView(spriteImage); //make image viewable
+        this.imageView.relocate(x,y); //relocates node into given x,y location
+        this.imageView.setRotate(r);
+
+        this.width = spriteImage.getWidth();
+        this.height = spriteImage.getHeight();
+
+        this.canMove = true;
     }
+
+    public Image getSpriteImage() {
+        return spreiteImage;
+    }
+
+    public ImageView getSpriteImageView () {
+        return spriteImageView;
+    }
+
+    public Pane getLayer() {
+        return layer;
+    }
+
+    public double getXComp() {
+        return xcomp;
+    }
+
+    public double getYComp() {
+        return ycomp;
+    }
+
+    public double getRComp() {
+        return rcomp;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public double getDr() {
+        return dr;
+    }
+
+    /*
+    /This method adds the image of a sprite to the Pane (layer)
+    */
+    public void addToLayer() {
+        this.layer.getChildren().add(this.spriteImageView);
+    }
+
+    public void move() {
+        if(!canMove) {
+            return;
+        } else {
+            x += dx;
+            y += dy;
+            r += dr;
+        }
+    }
+
 }
