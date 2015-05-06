8b17cc7bcabefb035dbb55d74454c8eb768df380
Fuad Hasbun <fhasbn3@gatech.edu>
2015-05-06 18:44:35 -0400

    Added inner and outer rectangles to the healthBar

--- HealthBar.java
+++ HealthBar.java
@@ -5,7 +5,23 @@ import javafx.scene.shape.StrokeType;
 
 public class HealthBar extends Pane {
 
+    private Rectangle outerRectangle;
+    private Rectangle innerRectangle;
+    private double height = 10;
+    private double outerWidth = 60;
+    private double innerWidth = 40;
+    private double x = 0;
+    private double y = 0;
+
     public HealthBar() {
+
+        outerRectangle = new Rectangle(x, y, outerWidth, height);
+        outerRectangle.setStroke(Color.BLACK);
+        outerRectangle.setStrokeWidth(2);
+        outerRectangle.setStrokeType(StrokeType.OUTSIDE);
+        outerRectangle.setFill(Color.RED);
+
+        innerRectangle = new Rectangle(x, y, innerWidth, )
 
     }
 }
